109ca5e62d2578277cc2c95f462da1a1899a7a8a
Andre Claudino <andre.claudino@b2wdigital.com>
2021-02-04 16:57:43 -0300

    Create TableMetada

--- src/gcloud/bigquery/table/create_entities.rs
+++ src/gcloud/bigquery/table/create_entities.rs
@@ -1,77 +1,53 @@
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct TableMetadata {
-
-	// The user-friendly name for the table.
-	pub name: String,
-	// Output-only location of the table, based on the encapsulating dataset.
-	pub location: String,
-	// The user-friendly description of the table.
-	pub description: String,
-	// The table schema. If provided on create, ViewQuery must be empty.
-	pub schema: Schema,
-	// If non-nil, this table is a materialized view.
-	pub materialized_view: MaterializedViewDefinition,
-	// The query to use for a logical view. If provided on create, Schema must be nil.
-	pub view_query: String,
-	// Use Legacy SQL for the view query.
-	// At most one of UseLegacySQL and UseStandardSQL can be true.
-	pub use_legacy_sql: bool,
-	// Use Standard SQL for the view query. The default.
-	// At most one of UseLegacySQL and UseStandardSQL can be true.
-	// Deprecated: use UseLegacySQL.
-	pub use_standard_sql: bool,
-	// If non-nil, the table is partitioned by time. Only one of
-	// time partitioning or range partitioning can be specified.
+	pub schema: Option<Vec<SchemaField>>,
 	pub time_partitioning: TimePartitioning,
-	// If non-nil, the table is partitioned by integer range.  Only one of
-	// time partitioning or range partitioning can be specified.
-	pub range_partitioning: RangePartitioning,
-	// If set to true, queries that reference this table must specify a
-	// partition filter (e.g. a WHERE clause) that can be used to eliminate
-	// partitions. Used to prevent unintentional full data scans on large
-	// partitioned tables.
-	pub require_partition_filter: bool,
+	clustering: Clustering
+}
 
-	// Clustering specifies the data clustering configuration for the table.
-	pub clustering: Clustering,
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SchemaField {
+	name: String,
+	
+	#[serde(rename="type")]
+	_type: FieldType,
+	mode: SchemaFieldMode,
+	description: String,
+	policy_tags: Vec<String>,
+	fields: Option<Vec<SchemaField>>,
+}
 
-	// The time when this table expires. If set, this table will expire at the
-	// specified time. Expired tables will be deleted and their storage
-	// reclaimed. The zero value is ignored.
-	pub expiration_time chrono::
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "UPPERCASE", untagged)]
+pub enum FieldType {
+	String,
+	Bytes,
+	Integer,
+	Float,
+	Boolean,
+	Timestamp,
+	Date,
+	Time,
+	DateTime,
+	Geography,
+	Numeric,
+	BigNumeric,
+	Struct
+}
 
-	// User-provided labels.
-	pub Labels map[string]string,
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct TimePartitioning {
+	#[serde(rename="type")]
+	_type: String,
+	expirationMs: String,
+	field: Option<String>,
+}
 
-	// Information about a table stored outside of BigQuery.
-	pub ExternalDataConfig *ExternalDataConfig,
-
-	// Custom encryption configuration (e.g., Cloud KMS keys).
-	pub EncryptionConfig *EncryptionConfig,
-
-	pub FullID           string, // An opaque ID uniquely identifying the table.
-	pub Type             TableType,
-	pub CreationTime     time.Time,
-	pub LastModifiedTime time.Time,
-
-	// The size of the table in bytes.
-	// This does not include data that is being buffered during a streaming insert.
-	pub NumBytes int64,
-
-	// The number of bytes in the table considered "long-term storage" for reduced
-	// billing purposes.  See https://cloud.google.com/bigquery/pricing#long-term-storage
-	// for more information.
-	pub NumLongTermBytes int64,
-
-	// The number of rows of data in this table.
-	// This does not include data that is being buffered during a streaming insert.
-	pub NumRows uint64,
-
-	// Contains information regarding this table's streaming buffer, if one is
-	// present. This field will be nil if the table is not being streamed to or if
-	// there is no data in the streaming buffer.
-	pub StreamingBuffer *StreamingBuffer,
-
-	// ETag is the ETag obtained when reading metadata. Pass it to Table.Update to
-	// ensure that the metadata hasn't changed since it was read.
-	pub ETag string,
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Clustering {
+	fields: Vec<String>,
 }
